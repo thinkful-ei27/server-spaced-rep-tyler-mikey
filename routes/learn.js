0dee833884097c81403579fe5a0977578c7a68cd
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-18 17:16:25 -0400

    word list v1 seeded to db

--- routes/learn.js
+++ routes/learn.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-router.post('/' , (req,res,next)=> {
+router.post('/', (req, res, next) => {
 
-})
+
+
+  
+});
+
 module.exports = router;
