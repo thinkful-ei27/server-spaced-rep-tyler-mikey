09e51912333a5cda8ce9f9eebb7a997d129f1eaf
mikeymanoguerra <mikeymanoguerra@gmail.com>
2019-03-19 14:37:15 -0400

    post endpoint for updating list working

--- routes/learn.js
+++ routes/learn.js
@@ -16,8 +16,43 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  
+  const userId = req.user.id;
+  const { germanWord, correct } = req.body;
 
+  if (typeof correct !== 'boolean') {
+    const err = new Error('Did not include result boolean');
+    err.status = 400;
+    return next(err);
+
+  }
+  return User.findOne({ _id: userId })
+    .then(user => {
+      let wordList = user.words;
+      if (wordList[0].germanWord !== germanWord) {
+        const err = new Error('User word does not match current DB word');
+        err.status = 400;
+        return next(err);
+      }
+
+      let testedWord = {
+        germanWord: wordList[0].germanWord,
+        englishWord: wordList[0].englishWord
+
+      };
+
+      if (correct) {
+        let newMvalue = wordList[0].Mvalue + 1;
+        testedWord.Mvalue = newMvalue;
+      } else testedWord.Mvalue = 1;
+
+      const newWordList = [...wordList.slice(1), testedWord];
+
+      return User.findOneAndUpdate({ _id: userId }, { $set: { words: newWordList } })
+        .then(() => {
+          res.sendStatus(204);
+        })
+        .catch(err => next(err));
+    });
 });
 
 module.exports = router;
